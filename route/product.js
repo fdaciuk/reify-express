9a8773fb0391dcb87670740d2c7b32b497216fc6
Fernando Daciuk <f.daciuk@gmail.com>
2017-10-26 10:49:50 -0200

    Add @std/esm

--- route/product.js
+++ route/product.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import Controller from '../controller/product'
+import * as Controller from '../controller/product'
 let App = express.Router()
 
 App.route('/').get(Controller.index)
