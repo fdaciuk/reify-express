7742b680cdfa0685e7e53d547058eb683633d4f3
Paiva <contato@gabrielpaiva.com>
2017-10-25 23:04:32 -0200

    Remove whitespaces

--- route/product.js
+++ route/product.js
@@ -2,10 +2,6 @@ import express from 'express'
 import Controller from '../controller/product'
 let App = express.Router()
 
-
-App
-	.route('/')
-	.get(Controller.index)
-
+App.route('/').get(Controller.index)
 
 export default App
